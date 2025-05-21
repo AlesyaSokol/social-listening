a6d5bcc1580547a93d341cf49eab65e467c01fd3
AlesyaSokol <sokolalesja@gmail.com>
2025-05-21 14:43:04 +0200

    fix

--- model/run_clustering.py
+++ model/run_clustering.py
@@ -36,7 +36,7 @@ def run_clustering_for_date_range(start_date, end_date):
 
 if __name__ == "__main__":
     # Set date range
-    start_date = datetime(2025, 5, 14)
+    start_date = datetime(2025, 5, 12)
     end_date = datetime(2025, 5, 19)
     
     run_clustering_for_date_range(start_date, end_date) 
