3e8eef4c4670631149cc0a6bfc80fde7b4eca7a4
tikseniya <tikseniya@gmail.com>
2025-05-27 17:35:30 +0200

    test

--- database/main.py
+++ database/main.py
@@ -140,9 +140,9 @@ def ProcessWithToken(ids_and_dates, token, token_id):
                 update_date = posts[-1]['date']
                 counter += WriteToDB(posts, owner_id)
                 
-            print(datetime.fromtimestamp(update_date, tz=timezone.utc) == last_date)
+            print(datetime.fromtimestamp(update_date, tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S') == last_date)
             db.add_last_upd(owner_id, datetime.fromtimestamp(update_date, tz=timezone.utc))
-            print("Обновлена дата для:", owner_id, "Новая дата:", datetime.fromtimestamp(posts[0]['date']))
+            print("Обновлена дата для:", owner_id, "Новая дата:", update_date)
 
         print(f"Всего обработано {counter} постов для ID: {owner_id}")
 
